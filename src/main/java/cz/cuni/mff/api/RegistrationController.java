391a79324fbc85774f06667d33a9af2bbe002652
Adam Hornáček <adam.hornacek@icloud.com>
2017-02-18 20:14:15 +0100

    Initial commit

---
+++ src/main/java/cz/cuni/mff/api/RegistrationController.java
@@ -0,0 +1,14 @@
+package cz.cuni.mff.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @RequestMapping("/register")
+    public void register() {
+
+    }
+
+}
