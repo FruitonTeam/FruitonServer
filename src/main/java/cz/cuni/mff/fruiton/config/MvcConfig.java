839cdd053ba815c96cd7ce306e1a0d47620f4db5
Adam Hornáček <adam.hornacek@icloud.com>
2017-11-28 17:35:04 +0100

    SWP-214 Images on the web are broken
    Initial commit

--- src/main/java/cz/cuni/mff/fruiton/config/MvcConfig.java
+++ src/main/java/cz/cuni/mff/fruiton/config/MvcConfig.java
@@ -18,7 +18,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public final void addResourceHandlers(final ResourceHandlerRegistry registry) {
         // add mapping to static resources and image directory
         registry.addResourceHandler("/img/**")
-                .addResourceLocations(StorageUtils.getImageRoot().toURI().toString(), "classpath:/static/img/");
+                .addResourceLocations("classpath:/static/img/", StorageUtils.getImageRoot().toURI().toString());
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
     }
