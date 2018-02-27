88e308c51f9011b824999b1508e47bd5a847fa2b
Adam Hornáček <adam.hornacek@icloud.com>
2018-02-27 15:26:50 +0100

    SWP-455 web: add possibility to download the game
    Initial commit

--- src/main/java/cz/cuni/mff/fruiton/config/MvcConfig.java
+++ src/main/java/cz/cuni/mff/fruiton/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package cz.cuni.mff.fruiton.config;
 
+import cz.cuni.mff.fruiton.component.util.ReleasesHelper;
 import cz.cuni.mff.fruiton.util.StorageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -10,18 +12,35 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+    private static final Logger logger = Logger.getLogger(MvcConfig.class.getName());
+
+    private ReleasesHelper releasesHelper;
+
+    @Autowired
+    public MvcConfig(final ReleasesHelper releasesHelper) {
+        this.releasesHelper = releasesHelper;
+    }
+
     @Override
     public final void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        // add mapping to static resources and avatar directory
+        // add mapping to static resources, avatar and releases directory
         registry.addResourceHandler("/**")
                 .addResourceLocations(new ResourceProperties().getStaticLocations());
         registry.addResourceHandler("/avatar/**")
                 .addResourceLocations(StorageUtils.getImageRoot().toURI().toString());
+        if (releasesHelper.isReleasesPathCorrect()) {
+            registry.addResourceHandler("/releases/**")
+                    .addResourceLocations(releasesHelper.getReleasesPath());
+        } else {
+            logger.log(Level.INFO, "Releases directory {0} is not correct, ignoring", releasesHelper.getReleasesPath());
+        }
     }
 
     @Override
